e973bd49d1100847ff6165337175584c09e4b3e7
Revya <revya25@gmail.com>
2024-03-05 06:24:17 +0800

    Initial Commit

---
+++ Main/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Node nodeA = new Node("A");
+
+List<Node> nodeList =
+
+public class Node(String NodeName)
+{
+	public List<(Node adjacent, int distance)> AdjacentNodes { get; set; }
+}
