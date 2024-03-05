14778b3b73ae4d370b6829d9de4c793758205e85
Revya <revya25@gmail.com>
2024-03-05 18:57:28 +0800

    Some cleanup

--- Main/DijkstraSolver.cs
+++ Main/DijkstraSolver.cs
@@ -9,32 +9,32 @@ public class DijkstraComputer
 
     public ShortestPathData ShortestPath(Node from, Node to, List<Node> nodes)
     {
-        Dictionary<Node, DijkstraMap> mapping = createMapping(from, to, nodes, _edges);
+        Dictionary<Node, DijkstraMap> mapping = createMapping(from, nodes, _edges);
 
         List<Node> nodeOrder = new List<Node>();
-        for(Node currentNode = to; currentNode != from; currentNode = mapping[currentNode].Parent)
+        for (Node currentNode = to; currentNode != from; currentNode = mapping[currentNode].Parent)
         {
-            if(mapping[currentNode].Parent is null) break;
+            if (mapping[currentNode].Parent is null) break;
             nodeOrder.Add(currentNode);
-        } 
+        }
         nodeOrder.Add(from);
         nodeOrder.Reverse();
 
         ShortestPathData output = new ShortestPathData(nodeOrder.Select(x => x.NodeName).ToList(), mapping[to].Distance);
-        
+
         return output;
     }
 
-    private Dictionary<Node, DijkstraMap> createMapping(Node from, Node to, List<Node> nodes, List<Edge> edges)
+    private Dictionary<Node, DijkstraMap> createMapping(Node from, List<Node> nodes, List<Edge> edges)
     {
-        Dictionary<Node, DijkstraMap> mapping = nodes.Select(node => 
+        Dictionary<Node, DijkstraMap> mapping = nodes.Select(node =>
             (Node: node, Mapping: node == from ? new DijkstraMap(0, null) : new DijkstraMap(int.MaxValue, null)))
             .ToDictionary(pair => pair.Node, pair => pair.Mapping);
 
         List<Node> finalized = new List<Node>();
-        List<Node> notFinalized = new List<Node>{from};
+        List<Node> notFinalized = new List<Node> { from };
 
-        while(notFinalized.Count > 0)
+        while (notFinalized.Count > 0)
         {
             Node minimalDistanceNode = notFinalized.Select(node => (Node: node, Mapping: mapping[node]))
                                                 .MinBy(x => x.Mapping.Distance).Node;
@@ -43,12 +43,13 @@ public class DijkstraComputer
 
             edges.Where(edge => edge.Start == minimalDistanceNode && !finalized.Contains(edge.End))
                 .ToList()
-                .ForEach(edge => {
+                .ForEach(edge =>
+                {
                     Node end = edge.End;
                     int currentDistance = mapping[end].Distance;
                     int edgeDistance = edge.Distance;
                     int newDistance = mapping[minimalDistanceNode].Distance + edgeDistance;
-                    if(newDistance < currentDistance)
+                    if (newDistance < currentDistance)
                     {
                         mapping[end] = mapping[end] with { Distance = newDistance, Parent = minimalDistanceNode };
                         notFinalized.Add(end);
